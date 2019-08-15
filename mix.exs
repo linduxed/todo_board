29d45e5c3837eeb70c0ef97dc3ce6e72dde578ce
Mikael Muszynski <linduxed@gmail.com>
2019-08-15 23:44:27 +0200

    Increase Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TodoBoard.MixProject do
     [
       app: :todo_board,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

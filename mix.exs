87d42f4a6bbf3a01dcb00de25153e08052be88c4
Mikael Muszynski <linduxed@gmail.com>
2020-10-12 10:35:10 +0200

    WIP: add factories and implement tests

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule TodoBoard.MixProject do
       app: :todo_board,
       version: "0.1.0",
       elixir: "~> 1.9",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
@@ -27,6 +28,7 @@ defmodule TodoBoard.MixProject do
     [
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:ex_machina, "~> 2.4", only: :test},
       {:glicko, github: "linduxed/elixir-glicko", branch: "develop"},
       {:logger_file_backend, "~> 0.0.11"},
       {:ratatouille, "~> 0.5.0"},
@@ -40,4 +42,7 @@ defmodule TodoBoard.MixProject do
       test: "test --no-start"
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
--- test/todo_board/app/rating/update/normal_test.exs
+++ test/todo_board/app/rating/update/normal_test.exs
@@ -5,6 +5,7 @@ defmodule TodoBoard.App.Rating.Update.NormalTest do
 
   alias TodoBoard.App.Rating.Update.Normal
   alias TodoBoard.App.Base.Model
+  alias TodoBoard.Factory
 
   require Model
 
@@ -15,16 +16,103 @@ defmodule TodoBoard.App.Rating.Update.NormalTest do
       %{event_hover_left: {:event, %{key: key(:arrow_left)}}}
     end
 
-    test "changes `hover` field on todo panels"
-    test "does nothing if left todo panel is hovered over"
+    test "changes `hover` field on todo panels", context do
+      todos = Factory.build_list(3, :todo, %{status: "pending"})
+
+      model =
+        Factory.build(:model,
+          rating_tab_data:
+            Factory.build(
+              :rating_tab_data,
+              choice: :none,
+              todos: todos
+            ),
+          todos: todos
+        )
+
+      new_model = Normal.update(model, context.event_hover_left)
+
+      assert new_model ==
+               model
+               |> update_tab_data(fn rating_data ->
+                 rating_data
+                 |> put_in([:left, :hover], true)
+                 |> put_in([:right, :hover], false)
+                 |> put_in([:draw, :hover], false)
+                 |> put_in([:choice], :left)
+               end)
+    end
+
+    test "does nothing if left todo panel is hovered over", context do
+      todos = Factory.build_list(3, :todo, %{status: "pending"})
+
+      model =
+        Factory.build(:model,
+          rating_tab_data:
+            Factory.build(
+              :rating_tab_data,
+              choice: :left,
+              todos: todos
+            ),
+          todos: todos
+        )
+
+      new_model = Normal.update(model, context.event_hover_left)
+
+      assert new_model == model
+    end
   end
 
   describe "update/2 - Hover right todo" do
     setup do
       %{event_hover_right: {:event, %{key: key(:arrow_right)}}}
     end
-    test "changes `hover` field on todo panels"
-    test "does nothing if right todo panel is hovered over"
+
+    test "changes `hover` field on todo panels", context do
+      todos = Factory.build_list(3, :todo, %{status: "pending"})
+
+      model =
+        Factory.build(:model,
+          rating_tab_data:
+            Factory.build(
+              :rating_tab_data,
+              choice: :none,
+              todos: todos
+            ),
+          todos: todos
+        )
+
+      new_model = Normal.update(model, context.event_hover_right)
+
+      assert new_model ==
+               model
+               |> update_tab_data(fn rating_data ->
+                 rating_data
+                 |> put_in([:left, :hover], false)
+                 |> put_in([:right, :hover], true)
+                 |> put_in([:draw, :hover], false)
+                 |> put_in([:choice], :right)
+               end)
+    end
+
+    test "does nothing if right todo panel is hovered over", context do
+      todos = Factory.build_list(3, :todo, %{status: "pending"})
+
+      model =
+        Factory.build(:model,
+          rating_tab_data:
+            Factory.build(
+              :rating_tab_data,
+              choice: :right,
+              todos: todos
+            ),
+          todos: todos
+        )
+
+      new_model = Normal.update(model, context.event_hover_right)
+
+      assert new_model == model
+    end
   end
 
   describe "update/2 - Hover draw button" do
@@ -32,8 +120,50 @@ defmodule TodoBoard.App.Rating.Update.NormalTest do
       %{event_hover_draw: {:event, %{key: key(:arrow_down)}}}
     end
 
-    test "changes `hover` field on todo panels and on draw button"
-    test "does nothing if draw button is hovered over"
+    test "changes `hover` field on todo panels and on draw button", context do
+      todos = Factory.build_list(3, :todo, %{status: "pending"})
+
+      model =
+        Factory.build(:model,
+          rating_tab_data:
+            Factory.build(
+              :rating_tab_data,
+              choice: :none,
+              todos: todos
+            ),
+          todos: todos
+        )
+
+      new_model = Normal.update(model, context.event_hover_draw)
+
+      assert new_model ==
+               model
+               |> update_tab_data(fn rating_data ->
+                 rating_data
+                 |> put_in([:left, :hover], false)
+                 |> put_in([:right, :hover], false)
+                 |> put_in([:draw, :hover], true)
+                 |> put_in([:choice], :draw)
+               end)
+    end
+    test "does nothing if draw button is hovered over", context do
+      todos = Factory.build_list(3, :todo, %{status: "pending"})
+
+      model =
+        Factory.build(:model,
+          rating_tab_data:
+            Factory.build(
+              :rating_tab_data,
+              choice: :draw,
+              todos: todos
+            ),
+          todos: todos
+        )
+
+      new_model = Normal.update(model, context.event_hover_draw)
+
+      assert new_model == model
+    end
   end
 
   describe "update/2 - Choose hovered over todo" do
